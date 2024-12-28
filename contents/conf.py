077480d43e6ae9dc0129fda6504ed57602a9a128
Kazuya Takei <myself@attakei.net>
2024-12-28 11:23:02 +0900

    chore: Initial

---
+++ contents/conf.py
@@ -0,0 +1,18 @@
+# -- Project information
+project = "attakei pages"
+copyright = "2024, attakei"
+author = "attakei"
+release = "2025"
+
+# -- General configuration
+extensions = [
+    # Private
+    "pages",
+]
+templates_path = ["_templates"]
+exclude_patterns = []
+language = "ja"
+
+# -- Options for HTML output
+html_theme = "alabaster"
+html_static_path = ["_static"]
---
+++ src/pages/__init__.py
@@ -0,0 +1,8 @@
+"""Private project for website."""
+
+from sphinx.application import Sphinx
+from sphinx.util.typing import ExtensionMetadata
+
+
+def setup(app: Sphinx) -> ExtensionMetadata | None:
+    pass
