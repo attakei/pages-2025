97d5baa8e954805e8f090257bf166d365abd0ebf
Kazuya Takei <myself@attakei.net>
2024-12-31 21:28:27 +0900

    site: Show last updated

--- contents/conf.py
+++ contents/conf.py
@@ -28,6 +28,8 @@ html_logo = "_static/images/icon-attakei@2x.png"
 html_js_files = [
     "https://kit.fontawesome.com/9da28e60fd.js",
 ]
+html_last_updated_fmt = "%Y-%m-%d %H:%M:%S"
+html_last_updated_use_utc = True
 html_theme = "pages-2025"
 html_theme_options = {
     "bulmaswatch": "flatly",
