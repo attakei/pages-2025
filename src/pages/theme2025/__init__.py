7f96186ca7baab655a98a4826fca888769e940dd
Kazuya Takei <myself@attakei.net>
2025-01-06 02:22:25 +0900

    fix: Use custom templates

--- src/pages/theme2025/__init__.py
+++ src/pages/theme2025/__init__.py
@@ -3,14 +3,20 @@
 from pathlib import Path
 
 from sphinx.application import Sphinx
+from sphinx.config import Config
 
 here = Path(__file__).parent
+
+
+def setup_templates(app: Sphinx, config: Config):
+    config.templates_path.append(str(here / "templates"))
 
 
 def setup(app: Sphinx):  # noqa: D103
     app.add_html_theme("pages-2025", str(here))
     app.setup_extension("atsphinx.bulma")
     app.setup_extension("atsphinx.bulma.themes.bulma_basic")
+    app.connect("config-inited", setup_templates)
     return {
         "version": "0",
         "env_version": 1,
