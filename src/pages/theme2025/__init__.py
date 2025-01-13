9fd61f0c8304e8f2e674917f4313f5f7cb697062
Kazuya Takei <myself@attakei.net>
2025-01-13 13:43:56 +0900

    feat: Ref static path of theme

--- src/pages/theme2025/__init__.py
+++ src/pages/theme2025/__init__.py
@@ -8,15 +8,16 @@ from sphinx.config import Config
 here = Path(__file__).parent
 
 
-def setup_templates(app: Sphinx, config: Config):
-    config.templates_path.append(str(here / "templates"))
+def setup_resources(app: Sphinx, config: Config):
+    config.templates_path.insert(1, str(here / "templates"))
+    config.html_static_path.insert(1, str(here / "static"))
 
 
 def setup(app: Sphinx):  # noqa: D103
     app.add_html_theme("pages-2025", str(here))
     app.setup_extension("atsphinx.bulma")
     app.setup_extension("atsphinx.bulma.themes.bulma_basic")
-    app.connect("config-inited", setup_templates)
+    app.connect("config-inited", setup_resources)
     return {
         "version": "0",
         "env_version": 1,
